af663e59a70866d689e97843ff1df9fda0a92219
Hangil Ko <framenumber08@gmail.com>
2020-01-15 11:44:00 +0000

    Language Setting Progress 1

---
+++ src/L4Project/Assets/Scripts/DropdownManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropdownManager : MonoBehaviour
+{
+
+    public void HandleLanguageOption(int option)
+    {
+        if (option == 0)
+        {
+            Lean.Localization.LeanLocalization.CurrentLanguage = "English";
+        }
+        if (option == 1)
+        {
+            Lean.Localization.LeanLocalization.CurrentLanguage = "Spanish";
+        }
+        if (option == 2)
+        {
+            Lean.Localization.LeanLocalization.CurrentLanguage = "Chinese";
+        }
+    }
+}
